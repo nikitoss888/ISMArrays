cbe1b3e676c142408100a2de04761ae6951a4b68
Никита Олексейчук <nikitoss888@gmail.com>
2019-10-14 22:44:30 +0300

    Добавьте файлы проекта.

---
+++ Array1/Array1Prog.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using static System.Math;
+
+namespace Array1
+{
+    class Array1Prog
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Размер матрицы: "); int N = int.Parse(Console.ReadLine());
+            float[] mtx = new float[N]; int index;
+            Console.WriteLine("Введите члены матрицы по-очереди");
+            for (int i = 0; i < N; i++)
+                mtx[i] = float.Parse(Console.ReadLine());
+            Console.WriteLine("Сумма негативных чисел массива равна {0}\n", lessthan0sum(N, mtx));
+            Console.WriteLine("Наибольший член массива равен {0}, его индекс: {1}\n", maxElem(N, mtx, out index), index);
+            Console.WriteLine("Наибольший по модулю член массива равен {0}\n", absmax(N, mtx));
+            Console.WriteLine("Сумма индексов позитивных членов массива равна {0}\n", morethan0indsum(N, mtx));
+            Console.WriteLine("Количество целых чисел в массиве: {0}\n", intnums(N, mtx));
+            Console.ReadKey();
+        }
+        static float lessthan0sum(int N, float[] mtx)
+        {
+            float sum = 0;
+            for (int i = 0; i < N; i++)
+                if (mtx[i] < 0)
+                    sum += mtx[i];
+            return sum;
+        }
+        static float maxElem(int N, float[] mtx, out int ind)
+        {
+            float max = float.MinValue;
+            ind = 0;
+            for (int i = 0; i < N; i++)
+                if (mtx[i] > max)
+                {
+                    max = mtx[i];
+                    ind = i;
+                }
+            return max;
+        }
+        static float absmax(int n, float[] a)
+        {
+            float absmax = a[0];
+            for (int i = 1; i < n; i++)
+                if (Abs(absmax) < Abs(a[i]))
+                    absmax = a[i];
+            return absmax;
+        }
+        static int morethan0indsum(int n, float[] a)
+        {
+            int mt0is = 0;
+            for (int i = 0; i < n; i++)
+                if (a[i] > 0)
+                    mt0is += i;
+            return mt0is;
+        }
+        static int intnums(int n, float[] a)
+        {
+            int intn = 0;
+            for (int i = 0; i < n; i++)
+                if (a[i] % 1 == 0)
+                    intn++;
+            return intn;
+        }
+    }
+}
---
+++ Array2/Array2Prog.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array2
+{
+    class Array2Prog
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Размер матрицы: "); int N = int.Parse(Console.ReadLine());
+            float[] mtx = new float[N]; int index;
+            Console.WriteLine("Введите члены матрицы по-очереди");
+            for (int i = 0; i < N; i++)
+                mtx[i] = float.Parse(Console.ReadLine());
+            Console.WriteLine("Произведение элементов матрицы после минимального равно: {0}\n", multipl_1(N, mtx));
+            Console.WriteLine("Сумма элементов между первым отрицательным и вторым положительным равна: {0}\n", sum_1(N, mtx));
+            Console.WriteLine("Сумма элементов после первого нуля и перед последним равна: {0}\n", sum_2(N, mtx));
+            Console.WriteLine("Произведение элементов матрицы между минимальным и максимальным по модулю равно: {0}\n", multipl_2(N, mtx));
+            Console.ReadKey();
+        }
+        static float multipl_1(int n, float[] a)
+        {
+            int minindex = 0; float res = 1, min = float.MaxValue;
+            for (int i = 0; i < n; i++)
+                if (min > a[i])
+                {
+                    min = a[i];
+                    minindex = i + 1;
+                }
+            if (minindex >= n)
+                return 0;
+            else
+            {
+                for (; minindex < n; minindex++)
+                    res *= a[minindex];
+                return res;
+            }
+        }
+        static float sum_1(int n, float[] a)
+        {
+            int minindex = 0, maxindex = n, c = 0; float sum = 0;
+            for (int i = 0; i < n; i++)
+                if (a[i] < 0)
+                {
+                    minindex = i + 1;
+                    break;
+                }
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] > 0)
+                    c++;
+                if (c == 2)
+                {
+                    maxindex = i;
+                    break;
+                }
+            }
+            if (minindex > maxindex)
+            {
+                int o = minindex;
+                minindex = maxindex + 1;
+                maxindex = o - 1;
+            }
+            for (; minindex < maxindex; minindex++)
+                sum += a[minindex];
+            return sum;
+        }
+        static float sum_2(int n, float[] a)
+        {
+            int minind = 0, maxind = n, c = 0; float sum = 0;
+            for (int i = 0; i<n; i++)
+                if (a[i] == 0)
+                {
+                    c++;
+                    switch (c)
+                    {
+                        case 1:
+                            minind = i + 1;
+                            break;
+                        case 2:
+                            maxind = i;
+                            break;
+                    }
+                }
+            if (minind == 0 || maxind == n)
+            {
+                Console.WriteLine("Одного из нулей нет, дается сумма всех элементов/всех после первого нуля.\n");
+            }
+            if (minind > maxind)
+            {
+                int o = minind;
+                minind = maxind + 1;
+                maxind = o;
+            }
+            for (; minind < maxind; minind++)
+                sum += a[minind];
+            return sum;
+        }
+        static float multipl_2(int n, float[] a)
+        {
+            float res = 1, min = float.MaxValue, max = float.MinValue; int minind = 0, maxind = n;
+            for (int i = 0; i < n; i++)
+            {
+                if (max < Math.Abs(a[i]))
+                {
+                    max = a[i];
+                    maxind = i;
+                }
+                if (min > Math.Abs(a[i]))
+                {
+                    min = a[i];
+                    minind = i + 1;
+                }
+            }
+            if (minind > maxind)
+            {
+                int o = minind;
+                minind = maxind + 1;
+                maxind = o - 1;
+            }
+            for (; minind < maxind; minind++)
+                res *= a[minind];
+            return res;
+        }
+    }
+}
